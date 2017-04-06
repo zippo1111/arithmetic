b061967f2a321ffe2bfb68d54e7e061b0fd925f0
zippo1111 <zippo1111@yandex.ru>
2017-04-06 08:03:12 +0300

    Initial Commit

---
+++ ArithmeticProgression/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArithmeticProgression
+//
+//  Created by Magnolia on 06.04.17.
+//  Copyright © 2017 Magnolia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
